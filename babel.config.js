1770f4ea01195923e88b05eaca316c46b025df32
helitonoliveira <heliton.oliveira88@gmail.com>
2020-03-12 06:57:17 -0300

    Propriedades do React

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react"
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties'
+  ]
+};
+
+/**
+ * @babel-preset-env -> é responsável por alterar as funcionalidades 
+ * que o navegador ainda não entende.
+ */
+
+/**
+ * @babel-present-react -> é responsável por alterar as funcionalidades
+ * que o navegador ainda não entende do React.
+ */
